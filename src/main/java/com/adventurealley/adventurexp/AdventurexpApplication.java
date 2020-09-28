b3eaa0b8a8a924f3ca4f922ffc77c1f1b2eef802
rasmus povlsen <rasmus.alexander.povlsen@hotmail.dk>
2020-09-28 12:44:49 +0200

    Initial Spring Setup

---
+++ src/main/java/com/adventurealley/adventurexp/AdventurexpApplication.java
@@ -0,0 +1,15 @@
+package com.adventurealley.adventurexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventurexpApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AdventurexpApplication.class, args);
+    }
+
+}
