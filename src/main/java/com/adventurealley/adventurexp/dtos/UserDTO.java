57e2f4ab4e9efdec42585d2015b5b5d706a4c41b
Fyziik <Fyziikme@gmail.com>
2020-09-29 13:26:58 +0200

    close #5

--- src/main/java/com/adventurealley/adventurexp/dtos/UserDTO.java
+++ src/main/java/com/adventurealley/adventurexp/dtos/UserDTO.java
@@ -5,4 +5,36 @@ public class UserDTO {
     private String password;
     private String role;
     private String email;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- src/main/java/com/adventurealley/adventurexp/entities/Role.java
+++ src/main/java/com/adventurealley/adventurexp/entities/Role.java
@@ -2,5 +2,23 @@ package com.adventurealley.adventurexp.entities;
 
 public enum Role {
     EMPLOYEE,
-    CUSTOMER
+    CUSTOMER;
+
+    public static int getID(Role role) {
+        switch (role) {
+            case CUSTOMER: return 1;
+            case EMPLOYEE: return 0;
+            default: return -1;
+        }
+    }
+
+    public static Role parse(String role) {
+        switch (role) {
+            case "EMPLOYEE": return EMPLOYEE;
+            case "CUSTOMER": return CUSTOMER;
+            default: return null;
+        }
+
+
+    }
 }
