a1040096200938d3f6b31fea18761912719734a2
Rasmus Povlsen <rasmus.alexander.povlsen@hotmail.dk>
2020-10-04 11:00:09 +0200

    close #43

--- src/main/java/com/adventurealley/adventurexp/controllers/UserController.java
+++ src/main/java/com/adventurealley/adventurexp/controllers/UserController.java
@@ -1,22 +1,26 @@
 package com.adventurealley.adventurexp.controllers;
 
+import com.adventurealley.adventurexp.dtos.LoginDTO;
 import com.adventurealley.adventurexp.dtos.SignupDTO;
+import com.adventurealley.adventurexp.entities.User;
+import com.adventurealley.adventurexp.services.IUserService;
 import com.adventurealley.adventurexp.services.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
 @RestController
 public class UserController {
 
-    UserService us;
+    IUserService userService;
 
-    public UserController(UserService userController) {
-        this.us = userController;
+    public UserController(IUserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping("/login")
@@ -27,13 +31,19 @@ public class UserController {
     @PostMapping("/api/signup")
     ResponseEntity<String> signup(@RequestBody SignupDTO signupDTO) {
         //Create DTO and send down to DB through service layer
-        us.createUser(signupDTO);
+        userService.createUser(signupDTO);
         return new ResponseEntity<>("success", HttpStatus.CREATED);
     }
 
-    @PostMapping("/api/login")
-    String login() {
-        return "index";
+    @GetMapping("/api/login")
+    ResponseEntity<String> login(@RequestBody LoginDTO loginDTO, HttpSession session) {
+        User u = userService.authenticateUser(loginDTO);
+        if (u == null) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        } else {
+            session.setAttribute("user", u);
+            return new ResponseEntity<>(u.toString(), HttpStatus.OK);
+        }
     }
 
 }
