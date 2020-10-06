519df9c6985a91fffa1f924d6099f720faf51a9c
niko998c <niko998c@stud.kea.dk>
2020-10-06 13:44:04 +0200

    added buttons

--- src/main/resources/static/js/view-activity.js
+++ src/main/resources/static/js/view-activity.js
@@ -12,5 +12,16 @@ $(document).ready(() => {
             const card_template = $("#activity-card").text();
             const compiler = _.template(card_template);
             $('#activity-container').append(compiler({ activity }));
+
+        const getButtons = async () => {
+            console.log("test")
+            const button_template = $('#activity-button-template').text();
+
+            const compiler = _.template(button_template);
+            const user = await getUser();
+            $('#activity-button-container').append(compiler({ user }));
+        }
+
+        getButtons();
     }
 })
